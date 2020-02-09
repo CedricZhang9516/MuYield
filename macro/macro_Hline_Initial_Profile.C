3b67ec14b259e975bd326416fb73ea0e8ad93816
CedricZhang <zhangce9516@gmail.com>
2020-02-09 17:29:40 +0900

    update macro_Inital H Profile

--- macro/macro_Hline_Initial_Profile.C
+++ macro/macro_Hline_Initial_Profile.C
@@ -16,14 +16,17 @@ void macro_Hline_Initial_Profile(){
 	TH1F * hDecayZ = TreeToTH1F(tree,"z_dec",100,-10,2,
 		"z_dec<10 && z_dec>-8");
 	hDecayZ->SetTitle("H-line-Stopping_Z;Z[mm];N");
+
 	c->cd(2);
 	TH1F * hDecayX = TreeToTH1F(tree,"x_dec",200,-100,100,
 		"z_dec<10 && z_dec>-8");
 	hDecayX->SetTitle("H-line-Stopping_X;X[mm];N");
+
 	c->cd(3);
 	TH1F * hDecayY = TreeToTH1F(tree,"y_dec",100,-40,40,
 		"z_dec<10 && z_dec>-8");
 	hDecayY->SetTitle("H-line-Stopping_Y;Y[mm];N");
+
 	c->cd(4);
 	TH2F * hDecayXY = TreeToTH2F(tree,"y_dec:x_dec",200,-100,100,100,-40,40,
 		"z_dec<10 && z_dec>-8");
@@ -37,6 +40,45 @@ void macro_Hline_Initial_Profile(){
 	b->SetLineWidth(10);
 	b->Draw();
 
-	SaveTCanvas(c,"H-line_initial_Profile_50_40");
+	//SaveTCanvas(c,"H-line_initial_Profile_50_40");
+
+
+
+
+
+
+	TCanvas * c2 = NewTCanvas("c2","c2",500,500,1,1);
+
+	TH2F * hDecayYZ = TreeToTH2F(tree,"y_dec:z_dec",
+		100,-10,5,
+		100,-50,50,
+		//50,-40,40,
+		"z_dec<10 && z_dec>-8");
+	hDecayYZ->SetTitle("H-line-Stopping_YZ;Z[mm];Y[mm]");
+	hDecayYZ->Draw("colz");
+
+	SaveTCanvas(c2,"H-line_initial_Profile_2_ZY");
+
+/*
+	TH3F * hDecayXYZ = new TH3F("hDecayXYZ","hDecayXYZ;Z[mm];X[mm];Y[mm]",20,-10,2,100,-50,50,80,-40,40);
+
+	double x_dec, y_dec, z_dec;
+	tree->SetBranchAddress("x_dec", &x_dec);
+	tree->SetBranchAddress("y_dec", &y_dec);
+	tree->SetBranchAddress("z_dec", &z_dec);
+	int Nentries = tree->GetEntries();
+
+	for(int index_m=0; index_m < Nentries; index_m++){
+
+		tree->GetEntry(index_m);
+		if(z_dec<10 && z_dec>-8)hDecayXYZ->Fill(z_dec, x_dec, y_dec);
+
+	}
+
+	hDecayXYZ->Draw("lego2");
+*/
+
+
+
 
 }
