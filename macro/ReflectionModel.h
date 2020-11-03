6283d5ab2544edc6e32c433f7039362f99717ef7
CedricZhang <zhangce9516@gmail.com>
2020-11-03 16:10:44 +0900

    Reflection model

---
+++ macro/ReflectionModel.h
@@ -0,0 +1,7 @@
+#include "macro_MuYieldAna.h"
+#include "InsideLaserRegion.h"
+#include "../DiffusionModel.h"
+#include "MuYield_Class.C"
+
+void macro_ReflectionModel(TString filename = "MuYield.root");
+void ReflectionModel();
---
+++ macro/macro_ReflectionModel.cxx
@@ -0,0 +1,3 @@
+#include "macro_ReflectionModel.h"
+
+void macro_ReflectionModel(TString filename = "MuYield.root");
